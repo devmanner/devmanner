78b6b92dfdf30b1140abc128f0140508a6b8454f
mannerstedt <mannerstedt@3d39604e-803f-0410-b2ec-f7948a154be0>
2009-02-23 19:43:42 +0000

    Added shapes.

---
+++ erlang/ErlangMulticore/shapes.erl
@@ -0,0 +1,9 @@
+-module(shapes).
+-export([area/1]).
+
+area({square, X}) ->
+    X*X;
+area({triangle, Base, Height}) ->
+    Base * Height / 2;
+area({circle, Radius}) ->
+    math:pi() * Radius * Radius.
